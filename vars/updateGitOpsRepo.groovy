0ef3072b73c4201747a2c51acbf3f75e48693a15
Anadict Mathew <anadictmathew@gmail.com>
2026-04-28 21:24:50 +0530

    fix gitops push authentication

--- vars/updateGitOpsRepo.groovy
+++ vars/updateGitOpsRepo.groovy
@@ -1,17 +1,27 @@
 def call(String gitopsRepo, String branch, String imageRepo, String imageTag) {
-    sh """
-        rm -rf airline-platform-gitops
-        git clone -b ${branch} ${gitopsRepo}
-        cd airline-platform-gitops
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'github-creds',
+            usernameVariable: 'GIT_USERNAME',
+            passwordVariable: 'GIT_TOKEN'
+        )
+    ]) {
+        sh """
+            rm -rf airline-platform-gitops
 
-        sed -i 's|repository:.*|repository: ${imageRepo}|' backend/values.yaml
-        sed -i 's|tag:.*|tag: "${imageTag}"|' backend/values.yaml
+            git clone -b ${branch} https://${GIT_USERNAME}:${GIT_TOKEN}@github.com/AnadictMathew/airline-platform-gitops.git
 
-        git config user.name "Jenkins CI"
-        git config user.email "jenkins@techianadict.online"
+            cd airline-platform-gitops
 
-        git add backend/values.yaml
-        git commit -m "Update backend image tag to ${imageTag}" || echo "No changes to commit"
-        git push origin ${branch}
-    """
+            sed -i 's|repository:.*|repository: ${imageRepo}|' backend/values.yaml
+            sed -i 's|tag:.*|tag: "${imageTag}"|' backend/values.yaml
+
+            git config user.name "Jenkins CI"
+            git config user.email "jenkins@techianadict.online"
+
+            git add backend/values.yaml
+            git commit -m "Update backend image tag to ${imageTag}" || echo "No changes to commit"
+            git push origin ${branch}
+        """
+    }
 }
